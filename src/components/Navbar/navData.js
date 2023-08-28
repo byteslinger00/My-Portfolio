b5b714fe94adc61d16b4e2bd78c7697fef6c4a3c
Oleg <132208831+olegreimers@users.noreply.github.com>
2023-08-28 17:32:25 -0400

    Update navData.js

--- src/components/Navbar/navData.js
+++ src/components/Navbar/navData.js
@@ -19,16 +19,6 @@ export const navLinks = [
 
 export const socialLinks = [
   {
-    label: 'Twitter',
-    url: 'https://twitter.com/hamishMW',
-    icon: 'twitter',
-  },
-  {
-    label: 'Figma',
-    url: 'https://www.figma.com/@Hamish',
-    icon: 'figma',
-  },
-  {
     label: 'Github',
     url: 'https://github.com/HamishMW',
     icon: 'github',
